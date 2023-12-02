289c09ad020ec98fa8459a356f5aaa98a40eed33
calvindany <calvindany1102@gmail.com>
2023-12-02 13:40:53 +0700

    Upload Project

---
+++ src/springHibernate/App.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package springHibernate;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springHibernate.service.MahasiswaService;
+import springHibernate.view.MahasiswaView;
+
+/**
+ *
+ * @author ASUS
+ */
+
+
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static ApplicationContext applicationContext;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        applicationContext = new ClassPathXmlApplicationContext("classpath:spring-configuration.xml");
+        new MahasiswaView().setVisible(true);
+    }
+
+    public static MahasiswaService getMahasiswaService() {
+        return (MahasiswaService) applicationContext.getBean("MahasiswaService");
+    }    
+}
